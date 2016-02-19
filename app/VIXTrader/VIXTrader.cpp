5786918bc15022928309b6e5a643cdd2e7033bfa
ljw2602 <ljw2602@users.noreply.github.com>
2016-02-19 07:05:27 -0500

    debug lib/Price.cpp

--- app/VIXTrader/VIXTrader.cpp
+++ app/VIXTrader/VIXTrader.cpp
@@ -28,66 +28,66 @@ using namespace Series;
 
 
 VIXTrader::VIXTrader(const EODSeries& spx_db, const EODSeries& vix_db):
-  _spx_db(spx_db),
-  _vix_db(vix_db),
-  _invested_days(0)
+_spx_db(spx_db),
+_vix_db(vix_db),
+_invested_days(0)
 {
 }
 
 
 void VIXTrader::run(void) throw(TraderException)
 {
-  TA ta;
-  _invested_days = days(0);
-
-  Series::EODSeries::const_iterator vix_iter(_vix_db.begin());
-  advance(vix_iter, 100); // Skip first 100 bars to give enough room too BBANDS calculation
-  for( ; vix_iter != _vix_db.end(); ++vix_iter ) {
-
-    try {
-
-      // Calculate current invested days
-      if( ! _miPositions.open().empty() )
-        _invested_days = _invested_days + days(1);
-
-      // Calculate VIX BBANDS
-      TA::BBRes resBBANDS3 = ta.BBANDS(_vix_db.close(vix_iter, 100), 100, 3, 3);
-      TA::BBRes resBBANDS1 = ta.BBANDS(_vix_db.close(vix_iter, 100), 100, 1, 1);
-
-      // Check buy signal
-      if( _miPositions.open().empty() && vix_iter->second.close > resBBANDS3.upper_band[0] ) {
-        // Buy tomorrow's close
-        Series::EODSeries::const_iterator iter_entry = _spx_db.after(vix_iter->first);
-        if( iter_entry == _spx_db.end() ) {
-          cerr << "Can't open position after " << vix_iter->first << endl;
-          continue;
+    TA ta;
+    _invested_days = days(0);
+    
+    Series::EODSeries::const_iterator vix_iter(_vix_db.begin());
+    advance(vix_iter, 100); // Skip first 100 bars to give enough room too BBANDS calculation
+    for( ; vix_iter != _vix_db.end(); ++vix_iter ) {
+        
+        try {
+            
+            // Calculate current invested days
+            if( ! _miPositions.open().empty() )
+                _invested_days = _invested_days + days(1);
+            
+            // Calculate VIX BBANDS
+            TA::BBRes resBBANDS3 = ta.BBANDS(_vix_db.close(vix_iter, 100), 100, 3, 3);
+            TA::BBRes resBBANDS1 = ta.BBANDS(_vix_db.close(vix_iter, 100), 100, 1, 1);
+            
+            // Check buy signal
+            if( _miPositions.open().empty() && vix_iter->second.close > resBBANDS3.upper_band[0] ) {
+                // Buy tomorrow's close
+                Series::EODSeries::const_iterator iter_entry = _spx_db.after(vix_iter->first);
+                if( iter_entry == _spx_db.end() ) {
+                    cerr << "Can't open position after " << vix_iter->first << endl;
+                    continue;
+                }
+                
+                buy(_spx_db.name(), iter_entry->first, Price(iter_entry->second.open));
+            }
+            
+            // Check sell signal
+            if( ! _miPositions.open().empty() && vix_iter->second.close < resBBANDS1.upper_band[0] ) {
+                // Get next bar
+                Series::EODSeries::const_iterator iter_exit = _spx_db.after(vix_iter->first);
+                if( iter_exit == _spx_db.end() ) {
+                    cerr << "Can't close position after " << vix_iter->first << endl;
+                    continue;
+                }
+                
+                // Close all open positions at tomorrow's close
+                PositionSet ps = _miPositions.open();
+                for( PositionSet::const_iterator pos_iter = ps.begin(); pos_iter != ps.end(); ++pos_iter ) {
+                    PositionPtr pPos = (*pos_iter);
+                    close(pPos->id(), iter_exit->first, Price(iter_exit->second.open));
+                } // end of all open positions
+            }
+            
+        } catch( std::exception& e ) {
+            
+            cerr << e.what() << endl;
+            continue;
         }
-
-	buy(_spx_db.name(), iter_entry->first, Price(iter_entry->second.open));
-      }
-
-      // Check sell signal
-      if( ! _miPositions.open().empty() && vix_iter->second.close < resBBANDS1.upper_band[0] ) {
-        // Get next bar
-        Series::EODSeries::const_iterator iter_exit = _spx_db.after(vix_iter->first);
-        if( iter_exit == _spx_db.end() ) {
-          cerr << "Can't close position after " << vix_iter->first << endl;
-          continue;
-        }
-
-        // Close all open positions at tomorrow's close
-        PositionSet ps = _miPositions.open();
-        for( PositionSet::const_iterator pos_iter = ps.begin(); pos_iter != ps.end(); ++pos_iter ) {
-          PositionPtr pPos = (*pos_iter);
-	  close(pPos->id(), iter_exit->first, Price(iter_exit->second.open));
-        } // end of all open positions
-      }
-
-    } catch( std::exception& e ) {
-
-      cerr << e.what() << endl;
-      continue;
-    }
-
-  } // for each bar
+        
+    } // for each bar
 }
